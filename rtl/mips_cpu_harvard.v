1c85973b24c6664c481f33df0741fea63b76fcbe
chernheng <tch1358@gmail.com>
2020-11-28 06:47:19 -0800

    Working on CPU

--- rtl/mips_cpu_harvard.v
+++ rtl/mips_cpu_harvard.v
@@ -20,18 +20,68 @@ module mips_cpu_harvard(
     typedef enum logic[1:0] {
         FETCH = 2'b00,
         EXEC1 = 2'b01,
-        EXEC2 = 2'b10
+        EXEC2 = 2'b10,
+        HALTED = 2'b11
     } state_t;
+
     typedef enum logic[1:0] {
         R = 2'b00,
         I = 2'b01,
         J = 2'b10
     } instr_type_t;
+
     typedef enum logic[5:0] {
         ADDU = 6'b100001,
-        EXEC1 = 2'b01,
-        EXEC2 = 2'b10
+        AND = 6'b100100,
+        DIV = 6'b011010,
+        DIVU = 6'b011011,
+        JALR = 6'b001001,
+        JR = 6'001000,
+        MTHI = 6'b010001,
+        MTLO = 6'b010011,
+        MULT = 6'b011000,
+        MULTU = 6'b011001,
+        OR = 6'b100101,
+        SLL = 6'b000000,
+        SLLV = 6'b000100,
+        SLT = 6'b101010,
+        SLTU = 6'b101011,
+        SRA = 6'b000011,
+        SRAV = 6'b000111,
+        SRL = 6'b000010,
+        SRLV = 6'b000110,
+        SUBU = 6'b100011,
+        XOR = 6'b100110,
+        MFHI = 6'b010000,
+        MFLO = 6'b010010
     } function_t;
+
+    typedef enum logic[5:0] {
+        ADDIU = 6'b001001,
+        ANDI = 6'b001100,
+        BEQ = 6'b000100,
+        BRANCH = 6'b000001,//4 instructions have the same opcode, determined by Rd
+        BGTZ = 6'b000111,
+        BLEZ = 6'b000110,
+        BNE = 6'b000101,
+        LB = 6'b100000,
+        LBU = 6'b100100,
+        LH = 6'b100001,
+        LHU = 6'b100101,
+        LUI = 6'b001111,
+        LW = 6'b100011,
+        LWL = 6'b100010,
+        LWR = 6'b100110,
+        ORI = 6'b001101,
+        SB = 6'b101000,
+        SH = 6'b101001,
+        SLTI = 6'b001010
+        SLTIU = 6'b001011,
+        SW = 6'b101011,
+        XORI = 6'b001110,
+        J = 6'b000010,
+        JAL = 6'b000011
+    } opcode_t;
 
     logic [1:0] state;
     logic [1:0] instr_type;
@@ -63,6 +113,29 @@ module mips_cpu_harvard(
     assign fn_code = instr_readdata[5:0];
     assign imm = instr_readdata[15:0];
     assign jump_addr = instr_readdata[25:0];
+
+    initial begin
+        state = HALTED;
+    end
+
+    always @(posedge clk) begin
+        if (!clk_enable) begin
+        end
+        else if (rst) begin
+            state <= FETCH;
+            pc <= 32'hBFC00000;
+            active <= 1;
+        end
+        else if(state == FETCH) begin
+            if(instr_readdata == 0) begin
+              state <= HALTED;
+              active <= 0;
+            end
+
+
+
+    end
+
 
 
 endmodule

7297df3083ff492fefced3adc9db101d5e811552
tianyilim <0.tianyi.lim@gmail.com>
2020-12-10 00:28:17 +0800

    Toolchain prototype done; update avalon slave impl

--- cross_compiler/assemble.sh
+++ cross_compiler/assemble.sh
@@ -18,18 +18,23 @@ mipsel-linux-gnu-gcc -S -g -O3 -nostdlib -march="mips1" -mfp32 -Wl,--build-id=no
 mipsel-linux-gnu-gcc -c -g -O3 -nostdlib -march="mips1" -mfp32 -Wl,--build-id=none -T linker_file.ld test.c -o test.mips.o
 mipsel-linux-gnu-gcc -g -O3 -nostdlib -march="mips1" -mfp32 -Wl,--build-id=none -T linker_file.ld test.c -o test.mips.elf
 
+# Generate hex file using objcopy and then formatted hexdump
+mipsel-linux-gnu-objcopy -O binary --only-section=.text test.mips.elf test.mips.asm.bin
+hexdump -v -e '4/1 "%02x " "\n"' test.mips.asm.bin > test.mips.asm.hex
+mipsel-linux-gnu-objcopy -O binary --only-section=.data test.mips.elf test.mips.data.bin
+hexdump -v -e '4/1 "%02x " "\n"' test.mips.data.bin > test.mips.data.hex
+
+
 # -f: Shows sections in the file
 # -S: Display source code intermixed with assembly code, if possible (if previously compiled with -g)
 # -d: Dipslays assembly code only
 # -j .data: Shows (unformatted) the content of the data section
 # -w: W I D E format (for modern terminals)
 # And we can use the output of this assembler to obtain the code that we need.
-
-echo "test.mips.asm assembly text dump" > test.mips.asm.txt
-echo "########## DATA SECTION ##########" >> test.mips.asm.txt
-mipsel-linux-gnu-objdump -s -j .data test.mips.elf >> test.mips.asm.txt
-echo "########## TEXT SECTION ##########" >> test.mips.asm.txt
-mipsel-linux-gnu-objdump -d test.mips.elf >> test.mips.asm.txt
+# Can verify by hand that this is the correct set of instructions
+# mipsel-linux-gnu-objdump -s -j .data test.mips.elf > test.mips.data.txt
+mipsel-linux-gnu-objdump -d test.mips.elf > test.mips.asm.txt
+# Process the code
 
 # debug
 # cat test.mips.asm.txt

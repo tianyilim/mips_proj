8d7b76ee3fd056468f6b6d7310e16e091dc64041
tianyilim <0.tianyi.lim@gmail.com>
2020-12-11 13:18:36 +0800

    Reformatting

--- parse_intermediate_files.sh
+++ parse_intermediate_files.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 for FILENAME in test/1-binary/*.intermediate.txt; do
+    [ -e "$FILENAME" ] || continue # Avoid case where there are no matches
     python3 test/parse_intermediates.py $FILENAME
 done

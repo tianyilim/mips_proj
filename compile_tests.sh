e251d8d337dfe17520188725fda620c3811f0313
tianyilim <0.tianyi.lim@gmail.com>
2020-12-12 15:21:43 +0800

    better testing-but does not yet run all

--- compile_tests.sh
+++ compile_tests.sh
@@ -3,11 +3,22 @@
 # Recompile assembler code
 g++ test/Assembler/src/assembler.cpp -o test/Assembler/src/assembler.out
 
+declare -i PASS=0
+declare -i FAIL=0
+
+cases=()
+
 if [ $# = 0 ]; then
     echo "No argument given, will run for all testcases"
+    cases+=("")
+else
+    for arg do
+        cases+=("$arg")
+    done
+fi
 
-    # Run through items to be assembled
-    for FILENAME in test/0-assembly/*.asm.txt; do
+for i in ${cases[@]}; do
+    for FILENAME in test/0-assembly/${i}*.asm.txt; do
         [ -e "$FILENAME" ] || continue # Avoid case where there are no matches
         BASENAME=`basename ${FILENAME}` # Name of test case
         BASENAME="${BASENAME%.*.*}"
@@ -20,34 +31,12 @@ if [ $# = 0 ]; then
             cat test/Assembler/src/test.txt # Debug outputs
             # Remove the assembled file (if it exists) if no success
             rm test/1-binary/$BASENAME.instr.hex
+            FAIL=$FAIL+1
         else
             cp test/Assembler/src/output.txt test/1-binary/$BASENAME.instr.hex # Write to output only if successful
+            PASS=$PASS+1
         fi
         # cat test/1-binary/$BASENAME.instr.hex # Debug outputs
     done
-
-else
-    for arg do 
-        echo "Assembling all tests starting with $arg"
-        for FILENAME in test/0-assembly/${arg}*.asm.txt; do
-            [ -e "$FILENAME" ] || continue # Avoid case where there are no matches
-            BASENAME=`basename ${FILENAME}` # Name of test case
-            BASENAME="${BASENAME%.*.*}"
-
-            python3 test/parse_comments.py ${FILENAME} # Get rid of comments
-
-            ./test/Assembler/src/assembler.out # Perform assembly
-            if [ $? != 0 ]; then
-                echo $FILENAME, $BASENAME.instr.hex # Debug outputs
-                cat test/Assembler/src/test.txt # Debug outputs
-                # Remove the assembled file (if it exists) if no success
-                test/1-binary/$BASENAME.instr.hex
-            else
-                cp test/Assembler/src/output.txt test/1-binary/$BASENAME.instr.hex # Write to output only if successful
-            fi
-
-            # cat test/1-binary/$BASENAME.instr.hex # Debug outputs
-
-        done
-    done
-fi
+done
+echo $FAIL, $PASS

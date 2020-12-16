17ee595db2b2fbf67ecdbad59f2ce82849a0f1ce
tianyilim <0.tianyi.lim@gmail.com>
2020-12-16 23:11:30 +0800

    Removed Depreceated folder

--- depreceated/cross_compiler/fib.c
+++
@@ -1,6 +0,0 @@
-int fib(int n){
-    if (n < 2){
-        return n;
-    }
-    return fib(n-1) + fib(n-2);
-}
--- depreceated/cross_compiler/test.c
+++
@@ -1,6 +0,0 @@
-int twentyFour = 24;
-int pi = 314121;
-
-int f(int n){
-    return twentyFour+pi+n;
-}
